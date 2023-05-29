da9942ef921c2f04735ef477ac274969bc80b2af
Joona Heinilä <joona.heinila@gmail.com>
2023-05-29 22:30:48 +0300

    osa 3 Node.js ja Express (3.1–3.3)

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+
+const persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1,
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2,
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3,
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World :)</h1>");
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  person ? res.json(person) : res.status(404).end();
+});
+
+app.get("/info", (req, res) => {
+  var dateTime = new Date();
+  res.send(
+    "<p>Phonebook includes info for " +
+      persons.length +
+      " people. </p><p>" +
+      dateTime
+  );
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
