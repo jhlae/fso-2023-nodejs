40bfc5094c89afcb4696745400451ccc2f03ee54
Joona Heinilä <joona.heinila@gmail.com>
2023-06-02 15:38:48 +0300

    chore: fix error 500

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(express.static("build"));
-
 app.use(morgan("tiny"));
 app.use(cors());
+app.use(express.json());
+app.use(express.static("build"));
 
 let persons = [
   {
