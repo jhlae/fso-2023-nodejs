eaf1d82320bd294040f4a120b6c2eea140a1509b
Joona Heinilä <joona.heinila@gmail.com>
2023-06-09 11:19:16 +0300

    osa 3: (3.13–3.14)

--- index.js
+++ index.js
@@ -2,36 +2,12 @@ const express = require("express");
 var morgan = require("morgan");
 const cors = require("cors");
 const PhonebookEntry = require("./models/phonebookentry");
-
 const app = express();
 
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
-
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World :)</h1>");
@@ -40,6 +16,12 @@ app.get("/", (req, res) => {
 /* 
 Operations for phonebook
 */
+
+// const generateRandId = () => {
+//   const maxID =
+//     persons.length > 0 ? Math.max(...persons.map((person) => person.id)) : 0;
+//   return maxID + 1;
+// };
 
 // Get all entries from db
 app.get("/api/persons", (req, res) => {
@@ -60,42 +42,17 @@ app.delete("/api/persons/:id", (req, res) => {
   res.status(204).end();
 });
 
-const generateRandId = () => {
-  const maxID =
-    persons.length > 0 ? Math.max(...persons.map((person) => person.id)) : 0;
-  return maxID + 1;
-};
-
 app.post("/api/persons", (req, res) => {
   const body = req.body;
 
-  if (!body.name || !body.number) {
-    return res.status(400).json({
-      error: "person is missing",
-    });
-  }
-  const person = {
+  const person = new PhonebookEntry({
     name: body.name,
     number: body.number,
-    id: generateRandId(),
-  };
+  });
 
-  const checkDuplicate = persons.find((p) => p.name === person.name);
-  // console.log(checkDuplicate);
-  if (checkDuplicate) {
-    return res.status(400).json({
-      error: "name must be unique",
-    });
-  } else {
-    persons = persons.concat(person);
-  }
-  console.log(persons);
-
-  res.json(persons);
-
-  // person.save().then((savedNote) => {
-  //   response.json(savedNote);
-  // });
+  person.save().then((savedEntry) => {
+    res.json(savedEntry);
+  });
 });
 
 app.get("/info", (req, res) => {
